456f4cc51d4ef0816db13b247fed576cf62bf452
Joe Kariuki <joe@devbrew.co>
2025-09-06 07:16:45 +0300

    refactor(components): remove ThemeSwitch and SearchButton from header
    - Comment out imports for `ThemeSwitch` and `SearchButton`
    - Remove their JSX usage from the header layout
    - Keep logo, navigation links, and mobile menu intact

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
+// import ThemeSwitch from './ThemeSwitch'
+// import SearchButton from './SearchButton'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -42,8 +42,8 @@ const Header = () => {
               </Link>
             ))}
         </div>
-        <SearchButton />
-        <ThemeSwitch />
+        {/* <SearchButton /> */}
+        {/* <ThemeSwitch /> */}
         <MobileNav />
       </div>
     </header>

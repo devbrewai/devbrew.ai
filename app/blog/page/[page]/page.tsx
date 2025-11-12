ef744a480826caa0bce35215fb7ad5379e6ddaab
Joe Kariuki <joe@devbrew.co>
2025-11-12 10:28:44 +0300

    refactor(blog): update paginated blog page to use Blog instead of Insight
    - Replace allInsights with allBlogs imports
    - Update generateStaticParams to use allBlogs.length
    - Update posts source to use allBlogs
    - Update page title from 'Insights' to 'Blogs'
    - Align with blog refactoring changes

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,19 +1,19 @@
 import ListLayout from '@/layouts/ListLayout'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import { allInsights } from 'contentlayer/generated'
+import { allBlogs } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
 
 const POSTS_PER_PAGE = 5
 
 export const generateStaticParams = async () => {
-  const totalPages = Math.ceil(allInsights.length / POSTS_PER_PAGE)
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
   const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
   return paths
 }
 
 export default async function Page(props: { params: Promise<{ page: string }> }) {
   const params = await props.params
-  const posts = allCoreContent(sortPosts(allInsights))
+  const posts = allCoreContent(sortPosts(allBlogs))
   const pageNumber = parseInt(params.page as string)
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
 
@@ -34,7 +34,7 @@ export default async function Page(props: { params: Promise<{ page: string }> })
       posts={posts}
       initialDisplayPosts={initialDisplayPosts}
       pagination={pagination}
-      title="Insights"
+      title="Blogs"
     />
   )
 }

8da2f11639de4390f2acd33d2b9dcc22a58b9828
Joe Kariuki <joe@devbrew.co>
2025-11-13 10:12:24 +0300

    feat(components): register Mermaid and use CustomPre in MDX components\n\n- Import and map Mermaid component for MDX usage\n- Replace pliny Pre with local CustomPre for code blocks\n- Enable Mermaid diagrams and customized code rendering in posts

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,16 +1,18 @@
 import TOCInline from 'pliny/ui/TOCInline'
-import Pre from 'pliny/ui/Pre'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import NewsletterForm from './NewsletterForm'
+import { CustomPre } from './Pre'
+import Mermaid from './Mermaid'
 
 export const components: MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
-  pre: Pre,
+  pre: CustomPre,
   table: TableWrapper,
   BlogNewsletterForm: NewsletterForm,
+  Mermaid,
 }

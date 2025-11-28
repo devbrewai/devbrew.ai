8ba111ffdef08ec7d6eed535ad46249213bb2682
Joe Kariuki <joe@devbrew.co>
2025-11-28 15:10:17 +0300

    fix(nav): update Case Studies link
    - Change Case Studies link from anchor (#case-studies) to full page route (/case-studies)

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Insights' },
-  { href: '/#case-studies', title: 'Case Studies' },
+  { href: '/case-studies', title: 'Case Studies' },
   { href: '/#services', title: 'Services' },
   { href: '/#about', title: 'About' },
   { href: '/contact', title: 'Contact' },
